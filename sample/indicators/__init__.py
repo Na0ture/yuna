876744fe36b9b34b8d8a8c86682b3ae55b06cd64
lvzhi <luolihaokong@hotmail.com>
2018-04-10 14:45:26 +0800

    常用分析方法，用于用户创建自己的作为参考

---
+++ sample/indicators/__init__.py
@@ -0,0 +1,11 @@
+from . import boll,ema,kdj,ma,macd,rsi,sma
+
+_all_indicators = {
+    'boll': boll.Boll,
+    'ema': ema.Ema,
+    'kdj': kdj.Kdj,
+    'ma': ma.Ma,
+    'macd': macd.Macd,
+    'rsi': rsi.Rsi,
+    'sma': sma.Sma,
+}
---
+++ sample/indicators/sma.py
@@ -0,0 +1,39 @@
+from yuna.core import TechnicalIndicator
+
+
+class Sma(TechnicalIndicator):
+    """
+    指数移动平均线（moving average）
+    算法来源：https://en.wikipedia.org/wiki/Moving_average
+    """
+
+    def __init__(self, data, days=6, factor=1, handle='off'):
+        self.days = days
+        self.weight_factor = factor / days
+        super().__init__(data, handle)
+
+    def _handle(self):
+        """self.ans = [ans]"""
+        ans_length = len(self.close)
+        for one in range(ans_length):
+            if one == 0:
+                self.ans.append(self.close[one])
+            else:
+                self.ans.append(self.weight_factor * self.close[one] + (1 - self.weight_factor) * self.ans[-1])
+
+    def __sub__(self, other):
+        ans, ans_length = [], len(self.ans)
+        for one in range(ans_length):
+            ans.append(self.ans[one] - other.ans[one])
+        return Sma(ans)
+
+    def __mul__(self, other):
+        return Sma(list(map(lambda x: x * other, self.ans)))
+
+    def __truediv__(self, other):
+        ans, ans_length = [], len(self.ans)
+        for one in range(ans_length):
+            if other.ans[one] == 0:
+                other.ans[one] = other.ans[one] + 0.000000001
+            ans.append(self.ans[one] / other.ans[one])
+        return Sma(ans)
