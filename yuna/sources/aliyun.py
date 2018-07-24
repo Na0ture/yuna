a4198eea885da054e5381ab80fb85739140e74a3
lvzhi <luolihaokong@hotmail.com>
2018-07-24 09:05:27 +0800

    更新网极API接口以及对应的test

--- yuna/sources/aliyun.py
+++ yuna/sources/aliyun.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import time
 from urllib.request import *
 import ssl
@@ -26,31 +26,33 @@ def retry(f):
 
 class AliyunSource(SourceSingleton):
     """
-    因aliyun商家提供的api缘故，获取财务数据跟k线数据分别各需要一条请求才能获取
+    因网极api样式缘故，获取基本面数据跟k线数据分别各需要一条请求才能获取
     """
 
-    host = 'https://stock.api51.cn'
-    path_kline = '/kline'
-    path_cwfx = '/f10'
+    host = 'https://data.api51.cn/apis'
+    path_kline = '/kline/'
+    path_cwfx = '/real/'
     method = 'GET'
-    query_kline = 'prod_code={}&' \
-                  'candle_period=6&' \
-                  'candle_mode=1&' \
-                  'fields=low_px,high_px,close_px,business_amount&' \
-                  'get_type=range&' \
-                  'start_date={}&' \
-                  'end_date={}'
-    query_cwfx = 'info={}_cwfx'
+    query_kline = 'stockcode={}&' \
+                  'market={}&' \
+                  'fqtype=1&' \
+                  'type=day&' \
+                  'token={}&' \
+                  'count={}&' \
+                  'date={}'
+    query_cwfx = 'stockcode={}&' \
+                 'market={}&' \
+                 'token={}'
     url_kline = host + path_kline + '?' + query_kline
     url_cwfx = host + path_cwfx + '?' + query_cwfx
     z = 0
 
     def packing(self, stocks, dates):
         stocks_list = super().change_stock(stocks)
-        from_query_date, to_query_date = self.__class__.datetime_to_date(self.__class__.validate_date(dates))
+        time_delta, to_query_date = self.__class__.datetime_to_date(self.__class__.calc_time_delta(self.__class__.validate_date(dates)))
         plane = Plane()
         for stock_name in stocks_list:
-            plane.append(self.__class__.request_to_truck(stock_name, from_query_date, to_query_date))
+            plane.append(self.__class__.request_to_truck(stock_name, time_delta, to_query_date))
         return plane
 
     @classmethod
@@ -62,21 +64,19 @@ class AliyunSource(SourceSingleton):
         return cls.dict_to_truck(stock_name, stock_kline_data, stock_cwfx_data)
 
     @classmethod
-    def datetime_to_date(cls, validity_dates):
-        return [i.strftime('%Y%m%d') for i in validity_dates]
+    def datetime_to_date(cls, time_delta):
+        return time_delta[0], time_delta[1].strftime('%Y%m%d')
 
     @classmethod
     def request_to_response(cls, stock_name, *dates):
         """
         :param stock_name: 股票名字，例如'002450.SZ'
-        :param dates: 从...到...日期，例如"20160531", "20160603"
+        :param dates: 一个包含截止日期前多少天以及截止日期的二元组，例如(5, datetime.datetime(2016, 6, 3))
         :return: 一个是股票k线json数据，一个是股票财务json数据，具体内容见test
         """
 
-        request_kline = Request(cls.url_kline.format(stock_name, *dates))
-        request_kline.add_header('Authorization', 'APPCODE ' + APP_CODE)
-        request_cwfx = Request(cls.url_cwfx.format(stock_name[:6]))
-        request_cwfx.add_header('Authorization', 'APPCODE ' + APP_CODE)
+        request_kline = Request(cls.url_kline.format(stock_name[:6], stock_name[7:], APP_CODE, *dates))
+        request_cwfx = Request(cls.url_cwfx.format(stock_name[:6], stock_name[7:], APP_CODE))
         ctx = ssl.create_default_context()
         ctx.check_hostname = False
         ctx.verify_mode = ssl.CERT_NONE
@@ -86,7 +86,7 @@ class AliyunSource(SourceSingleton):
     def json_kline_to_dict(cls, response):
         """
         :param response: 股票k线json数据
-        :return: 返回python类型，具体内容看test部分
+        :return: 返回dict类型，具体内容看test部分
         """
         content = response.read()
         return json.loads(content)
@@ -95,29 +95,15 @@ class AliyunSource(SourceSingleton):
     def json_cwfx_to_dict(cls, response):
         """
         :param response: 股票财务json数据
-        :return: 返回python类型，指向'gsgz'，具体内容看test部分
-
-        因财务数据过于庞大，暂不做测试，字段gsgz的内容范例如下：
-                "gsgz": [
-            {
-                "InnerCode": 36647,
-                "PB": 4.8866,
-                "PCF": -53.3646808,
-                "PS": 10.3098917,
-                "PE": 87.3309896,
-                "TradingDay": 1506700800000
-            }
-        ],
+        :return: 返回dict类型，具体内容看test部分
         """
 
         content = response.read()
-        return json.loads(content)['gsgz']
+        return json.loads(content)
 
     @classmethod
     def dict_to_truck(cls, stock_name, stock_kline_data, stock_cwfx_data):
         """
-        有时候阿里云会没有某些股票的pe，pb等相应的财务数据，它们存在的时候是一个数字，
-        不存在的时候，使用get不会报错，则返回0
 
         :param stock_name: 股票名字，例如'002450.SZ'
         :param stock_kline_data: 股票k线dict数据
@@ -127,17 +113,22 @@ class AliyunSource(SourceSingleton):
 
         truck = Truck()
         truck.extend("Code", [stock_name])
-        truck.extend("PE", [stock_cwfx_data[0].get('PE', 0)])
-        truck.extend("PB", [stock_cwfx_data[0].get('PB', 0)])
-        truck.extend("PS", [stock_cwfx_data[0].get('PS', 0)])
-        truck.extend("PCF", [stock_cwfx_data[0].get('PCF', 0)])
-        candle = stock_kline_data['data']['candle'][stock_name]
+        truck.extend("PE", [stock_cwfx_data['results'][0][-2]])
+        truck.extend("PB", [0])
+        truck.extend("PS", [0])
+        truck.extend("PCF", [0])
+        candle = stock_kline_data['results'][0]['array']
         for i in candle:
-            truck.append('Times', datetime.datetime.strptime(str(i[0]), '%Y%m%d'))
-            truck.append('Low', i[1])
-            truck.append('High', i[2])
-            truck.append('Close', i[3])
-            truck.append('Volume', i[4])
+            truck.append('Times', datetime.strptime(str(i[0]), '%Y%m%d'))
+            truck.append('Low', i[4])
+            truck.append('High', i[3])
+            truck.append('Close', i[2])
+            truck.append('Volume', i[6])
         cls.z += 1
         print(cls.z)
         return truck
+
+    @classmethod
+    def calc_time_delta(cls, time_list):
+        delta = time_list[1] - time_list[0]
+        return delta.days, time_list[1]
