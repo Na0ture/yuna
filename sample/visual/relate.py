debe241d4d206a20c59bfe9c257e3e4659ab05d2
lvzhi <luolihaokong@hotmail.com>
2018-07-31 08:24:40 +0800

    [视图模式增加图分析法]修正使用relate方法下，百分比显示不合理

--- sample/visual/relate.py
+++ sample/visual/relate.py
@@ -12,16 +12,16 @@ class Relate(VisualIndicator):
         super().__init__(data)
 
     def _handle(self):
-        fmt = '%.2f%%'
+        fmt = '%.0f%%'
         yticks = mtick.FormatStrFormatter(fmt)
-        fig, ax1 = plt.subplots(nrows=1, ncols=1, figsize=(10, 10))
+        fig, ax1 = plt.subplots(nrows=1, ncols=1, figsize=(10, 7))
         ax1.yaxis.set_major_formatter(yticks)
         ax1.xaxis.set_major_formatter(mdate.DateFormatter('%Y-%m-%d'))
         ax1.grid(True)
         for j in range(len(self.data)):
             exec(f"tmp0, y0 = self.data[{j}]['Close'], []")
             for k in range(len(self.data[j]['Close'])):
-                exec(f'y0.append(tmp0[{k}] / tmp0[0] - 1)')
+                exec(f'y0.append((tmp0[{k}] / tmp0[0] - 1) * 100)')
             exec(f"ax1.plot(self.data[{j}]['Times'], y0, label='{self.data[j]['Code'][0]}')")
         ax1.legend(loc=(0.65, 0.8))
         plt.show()
